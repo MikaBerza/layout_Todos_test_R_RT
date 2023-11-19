da95c3081b01046c0dc9ffee3d554b94088eca3a
Mika <mikberza@mail.ru>
2023-11-19 20:10:05 +0300

    Change (src): changing the location of component folders #1
    Изменить (src): изменение расположение папок компонентов #1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import HomePage from './components/pages/HomePage';
+import Header from './components/commons/headers/Header';
+import Footer from './components/commons/footers/Footer';
 import './App.css';
 
 function App() {
   return (
     <>
+      <Header title={'Todos'} />
       <HomePage />
+      <Footer />
     </>
   );
 }
--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import style from './homePage.module.css';
 
-import Header from '../commons/headers/Header';
-import Content from './content/Content';
-import Footer from '../commons/footers/Footer';
+import Control from '../section/sectionControl/Control';
+import EnteringTask from '../section/sectionEnteringTask/EnteringTask';
+import OutputTask from '../section/sectionOutputTask/OutputTask';
 
 function HomePage() {
   return (
     <>
       <div className={style.wrapper}>
-        <Header title={'Todos'} />
-        <Content />
-        <Footer />
+        <main className={style.inner}>
+          <Control />
+
+          <EnteringTask />
+
+          <OutputTask />
+        </main>
       </div>
     </>
   );
