836b615b5514fc552071e2af2071ae53184f6f51
Mika <mikberza@mail.ru>
2023-11-28 22:40:37 +0300

    Fix (Task.jsx): arguments, function parameters, hook(useCallback) #7 #7 -use of abbreviated syntax (JS) -removing arguments and function parameters -added a hook (useCallback)
    Исправить (Task.jsx): аргументы, параметры функции, хук(useCallback)  #7
    #7
    -использование сокращенного синтаксиса (JS)
    -удаление аргументов и параметров функции
    -добавлен хук(useCallback)

--- src/components/commons/task/Task/Task.jsx
+++ src/components/commons/task/Task/Task.jsx
@@ -11,11 +11,11 @@ import { setEditButton } from '../../../../redux/slices/buttonGroupSlice.js';
 
 import { writeToLocalStorage } from '../../../../utils/modules.js';
 import style from './task.module.css';
-// запись(date: calendarDate) означает, что значение свойства (date) теперь доступно под именем (calendarDate)
 
+// запись(date: calendarDate) означает, что значение свойства (date) теперь доступно под именем (calendarDate)
 const Task = React.memo(
   ({ id, note, date: calendarDate, sign, tick: checking }) => {
-    // 
+    //
     Task.propTypes = {
       id: PropTypes.string,
       note: PropTypes.string,
@@ -28,40 +28,43 @@ const Task = React.memo(
     const dispatch = useDispatch();
 
     // функция, обработать вход в режим редактирования задачи
-    const handleEntryIntoTaskEditingMode = (event, id) => {
-      /*
+    const handleEntryIntoTaskEditingMode = React.useCallback(
+      (event) => {
+        /*
       добавил проверку, является ли event.target элементом input,
       если да, то прекращаем выполнение функции. Проверка нужна,
       чтобы избежать перехода в режим редактирования при двойном клике по input.
       */
-      if (event.target.tagName.toLowerCase() === 'input') {
-        return;
-      }
-
-      const newTaskListData = taskListData.map((item) => {
-        if (item.id === id && item.tick !== true) {
-          // обновляем(устанавливаем) в поле textarea
-          dispatch(setTextareaMessage(item.note));
-          // показываем кнопку (редактировать) изменим ее состояние на (true), скрываем остальные кнопки
-          dispatch(setEditButton(true));
-          // скрываем все задачи, обновляя состояние
-          dispatch(setShowTasks(false));
-
-          // изменяем состояние поля с ключом (editing), с (false) на (true)
-          const newItem = { ...item, editing: true };
-          return newItem;
+        if (event.target.tagName.toLowerCase() === 'input') {
+          return;
         }
-        // возвращаем элемент массива без изменений если он не соответствует условию
-        return item;
-      });
-      // обновляем данные списка задач
-      dispatch(setTaskListData(newTaskListData));
-      // обновляем данные в localStorage
-      writeToLocalStorage(newTaskListData);
-    };
+
+        const newTaskListData = taskListData.map((item) => {
+          if (item.id === id && !item.tick) {
+            // обновляем(устанавливаем) в поле textarea
+            dispatch(setTextareaMessage(item.note));
+            // показываем кнопку (редактировать) изменим ее состояние на (true), скрываем остальные кнопки
+            dispatch(setEditButton(true));
+            // скрываем все задачи, обновляя состояние
+            dispatch(setShowTasks(false));
+
+            // изменяем состояние поля с ключом (editing), с (false) на (true)
+            const newItem = { ...item, editing: true };
+            return newItem;
+          }
+          // возвращаем элемент массива без изменений если он не соответствует условию
+          return item;
+        });
+        // обновляем данные списка задач
+        dispatch(setTaskListData(newTaskListData));
+        // обновляем данные в localStorage
+        writeToLocalStorage(newTaskListData);
+      },
+      [dispatch, id, taskListData]
+    );
 
     // функция, обработать изменение флажка
-    const handleCheckboxChange = (id) => {
+    const handleCheckboxChange = React.useCallback(() => {
       const newTaskListData = taskListData.map((item) => {
         if (item.id === id) {
           const newItem = { ...item, tick: !item.tick };
@@ -74,25 +77,20 @@ const Task = React.memo(
       dispatch(setTaskListData(newTaskListData));
       // записываем данные в localStorage
       writeToLocalStorage(newTaskListData);
-    };
+    }, [dispatch, id, taskListData]);
 
     // функция, обработать удаление задачи
-    const handleTaskRemove = (id) => {
-      // копируем список задач с помощью оператора spread
-      const copyTaskListData = [...taskListData];
+    const handleTaskRemove = React.useCallback(() => {
       // удаляем задачу из списка
-      const newTaskListData = copyTaskListData.filter((item) => item.id !== id);
+      const newTaskListData = taskListData.filter((item) => item.id !== id);
       // обновляем данные списка задач
       dispatch(setTaskListData(newTaskListData));
       // записываем данные в localStorage
       writeToLocalStorage(newTaskListData);
-    };
+    }, [dispatch, id, taskListData]);
 
     return (
-      <li
-        className={style.item}
-        onDoubleClick={(event) => handleEntryIntoTaskEditingMode(event, id)}
-      >
+      <li className={style.item} onDoubleClick={handleEntryIntoTaskEditingMode}>
         <div className={style.inner1}>
           <span className={`${checking ? style.completed : ''} ${style.text}`}>
             {note}
@@ -101,12 +99,12 @@ const Task = React.memo(
             className={style.checkbox}
             type='checkbox'
             checked={checking}
-            onChange={() => handleCheckboxChange(id)}
+            onChange={handleCheckboxChange}
           />
         </div>
         <div className={style.inner2}>
           <span className={style.date}>{calendarDate}</span>
-          <span className={style.remove} onClick={() => handleTaskRemove(id)}>
+          <span className={style.remove} onClick={handleTaskRemove}>
             {sign}
           </span>
         </div>
